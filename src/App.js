fde96978ef050ac4e40b5cbdd7b8132db12199f6
Vardaan Aashish <vardaan119@gmail.com>
2017-08-19 22:05:48 -0400

    added basic reducer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+
+  componentWillMount() {
+    // firebase setup
+    const config = {
+    apiKey: "AIzaSyADG3V0b5dk_trG5wdM3Ur-H0aY25CWbwc",
+    authDomain: "manager-6cf5f.firebaseapp.com",
+    databaseURL: "https://manager-6cf5f.firebaseio.com",
+    projectId: "manager-6cf5f",
+    storageBucket: "manager-6cf5f.appspot.com",
+    messagingSenderId: "538785704042"
+  };
+  firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
       </Provider>
     )
   }
 }
+
+export default App
